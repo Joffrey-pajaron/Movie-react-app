c2a2c9f0d913c368ea53fe8c9349ff5adc0e36d2
JoffreyxD <119108651+JoffreyxD@users.noreply.github.com>
2025-07-12 20:47:41 +0800

    Initial Vite React app

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './css/App.css'
+import Home from './pages/Home.jsx';
+import { Routes, Route } from 'react-router-dom';
+import Favorites from './pages/Favorites.jsx';
+import { MovieProvider } from "./contexts/MovieContext.jsx";
+import NavBar from './components/NavBar.jsx';
+
+
+function App() {
+ 
+
+
+  return (
+    <MovieProvider>
+      <NavBar />
+    <main className='main-content'>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </main>
+    </MovieProvider>
+  );
+}
+
+
+
+export default App
---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "15d1897cacbeda0a5b4f17e54cb0b2d3";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+  const data = await response.json();
+  return data.results;
+};
